f336b9096e4d4aab3cf48766115e5cea7d2d72ed
Sadhurahavan <109868957+Sadhurahavan5555@users.noreply.github.com>
2024-12-25 21:59:49 +0530

    Update and rename video.py to emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,10 +1,9 @@
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
-from google.colab.patches import cv2_imshow
 
-model = load_model('/content/model.h5')
-face_cascade = cv2.CascadeClassifier('/content/drive/MyDrive/Colab Notebooks/multiface_rec/haarcascade_frontalface_default.xml')
+model = load_model('model.h5')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 emotions_label = {0: 'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 4:'Neutral', 5:'Sad', 6:'Surprise'}
 cap = cv2.VideoCapture(0)
 
@@ -25,7 +24,7 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
 
-    cv2_imshow(frame)
+    cv2imshow(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
